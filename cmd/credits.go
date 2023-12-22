3cb82e584b98b208a570270e13f4d7a2452f8ea2
Deadloct <rad@deadloct.com>
2023-12-22 19:23:19 +1100

    Add PocketApple8104's guides

--- cmd/credits.go
+++ cmd/credits.go
@@ -33,7 +33,7 @@ func (c *Credits) Handle(sess *discordgo.Session, i *discordgo.InteractionCreate
 	content := `
 **Bot Credits**
 
-_Guide Authors_: 3riko, 5Rupees, a_poor_ninja, Adhesive81, Alysias, Antomanz, Ballbreaker, BillyIdol, Bisamratte, Blanquiito, ChubbyDaemon, Chuck, Colb, Commander, Crow, CyberMuffin, DarkHand6, Dispel1, Dracaris, Dude_WTF, Eliealsamaan85, Ember, fohpo, Fyra, Gagf, Gavx, Goku, Goolmuddy, Gylgymesh, Hæl (aka Hael in this bot), Huen11, ItsMBSCastillo, iWushock, JDizzle, Jermoshua, JoeBu, John_Hatten2, josiah_4, kruste, Lqd, MaxBrand99, McSploosh, Melody (Choco), MrRager, Mochi, n1ghtmaree, Olivernoko, Orcaaa, PAINisGOD93, RoastyChicken, ShawnBond, Sizz, Smolder, Special_Delivery, Tarnym, Techno, Toad, Tolton, TooT, UnseenAxes, VesaN, Winter, WRLD_EATR, Youreprettycute, ZENICKS, and ZombieSlayer13
+_Guide Authors_: 3riko, 5Rupees, a_poor_ninja, Adhesive81, Alysias, Antomanz, Ballbreaker, BillyIdol, Bisamratte, Blanquiito, ChubbyDaemon, Chuck, Colb, Commander, Crow, CyberMuffin, DarkHand6, Dispel1, Dracaris, Dude_WTF, Eliealsamaan85, Ember, fohpo, Fyra, Gagf, Gavx, Goku, Goolmuddy, Gylgymesh, Hæl (aka Hael in this bot), Huen11, ItsMBSCastillo, iWushock, JDizzle, Jermoshua, JoeBu, John_Hatten2, josiah_4, kruste, Lqd, MaxBrand99, McSploosh, Melody (Choco), MrRager, Mochi, n1ghtmaree, Olivernoko, Orcaaa, PAINisGOD93, PocketApple8104, RoastyChicken, ShawnBond, Sizz, Smolder, Special_Delivery, Tarnym, Techno, Toad, Tolton, TooT, UnseenAxes, VesaN, Winter, WRLD_EATR, Youreprettycute, ZENICKS, and ZombieSlayer13
 
 _Idea for Bot_: Trogburn
 
